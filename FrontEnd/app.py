4b86f0b10b22dd6fd1d15932ad75652df267ffe1
Steve Lopez <stivfunk2000@hotmail.com>
2025-03-10 17:59:38 -0500

    agregando pantalla con login

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,39 +1,81 @@
+from operator import itemgetter
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.schema import StrOutputParser
-from langchain.schema.runnable import Runnable
+
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
 from langchain.schema.runnable.config import RunnableConfig
+from langchain.memory import ConversationBufferMemory
+
+from chainlit.types import ThreadDict
+import chainlit as cl
+from chainlit.input_widget import Select, Slider
+
 from typing import cast
 
-import chainlit as cl
+
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
 
 
 @cl.on_chat_start
 async def on_chat_start():
-    model = ChatOpenAI(streaming=True)
-    prompt = ChatPromptTemplate.from_messages(
+    await cl.Message(content="How can I help you?").send()
+    settings = await cl.ChatSettings(
         [
-            (
-                "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
-            ),
-            ("human", "{question}"),
+            
+            Slider(
+                id="Temperature",
+                label="OpenAI - Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            )
         ]
-    )
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+    ).send()
+    value = settings["Model"]
+
+    cl.user_session.set("chat_history", [])
+
+    print(value)
+
+   
+
+
+
+@cl.on_chat_resume
+async def on_chat_resume(thread: ThreadDict):
+    cl.user_session.set("chat_history", [])
+
+    # user_session = thread["metadata"]
+    
+    for message in thread["steps"]:
+        if message["type"] == "user_message":
+            cl.user_session.get("chat_history").append({"role": "user", "content": message["output"]})
+        elif message["type"] == "assistant_message":
+            cl.user_session.get("chat_history").append({"role": "assistant", "content": message["output"]})
+
+
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+    chat_history = cl.user_session.get("chat_history")
 
-    msg = cl.Message(content="")
+    await cl.Message(content="Here is the ticket information!").send()
+    chat_history.append({"role": "admin", "content": message.content})
 
-    async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-    ):
-        await msg.stream_token(chunk)
-
-    await msg.send()
+    content = message.content
+    print(content)
+    
