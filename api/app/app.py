6dafa3b09ec3a138204cd0dd91b2ed4dedb3bed5
sergiosgalindo <sergiogalindo2010@hotmail.com>
2025-03-12 19:09:12 -0600

    test endpoint with model

--- api/app/app.py
+++ api/app/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify,  render_template, request, redirect, url_for
 #from config.swagger import swagger_configuration
 
-#from model.embeddings import Embedding
-#from model.prompt_retreiver import generate_response
 from model.index import generate_response
 
 app = Flask(__name__)
@@ -14,10 +12,10 @@ def message():
     
     data = request.json
     
-    #embedding = Embedding()
-    #print(embedding.embedding_model)
-    generate_response(data)
-    return jsonify(data)
+    print(data['query'])
+
+    return generate_response(data['query'])
+    #return jsonify(response_from_model)
     
 @app.get('/index')
 def hello():
