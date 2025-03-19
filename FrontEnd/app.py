85d88db9e076b5260a3486e1138813b57e6e9aed
Steve Lopez <stivfunk2000@hotmail.com>
2025-03-19 17:51:58 -0500

    frontend con logo

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -1,20 +1,18 @@
-from operator import itemgetter
 from langchain_openai import ChatOpenAI
-
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
-from langchain.memory import ConversationBufferMemory
-
-from chainlit.types import ThreadDict
-import chainlit as cl
-from chainlit.input_widget import Select, Slider
-
 from typing import cast
+import openai
+import requests
+import os
 
+import chainlit as cl
+HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 
-
+API_URL = "https://api-inference.huggingface.co/models/google/gemma-2b"
+headers = {"Authorization": "Bearer " + HUGGINGFACE_API_KEY}
 
 @cl.password_auth_callback
 def auth_callback(username: str, password: str):
@@ -26,56 +24,98 @@ def auth_callback(username: str, password: str):
         )
     else:
         return None
-
-
+    
 @cl.on_chat_start
 async def on_chat_start():
-    await cl.Message(content="How can I help you?").send()
-    settings = await cl.ChatSettings(
+    
+    # await cl.Message(content="Welcome to the chat!").send()
+
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages(
         [
-            
-            Slider(
-                id="Temperature",
-                label="OpenAI - Temperature",
-                initial=1,
-                min=0,
-                max=2,
-                step=0.1,
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+async def generate_chunks(text, chunk_size=2000):
+    """Yield chunks of text"""
+    for i in range(0, len(text), chunk_size):
+        yield text[i : i + chunk_size]
+
+# @cl.on_message
+# async def on_message(message: cl.Message):
+#     runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
+
+#     msg = cl.Message(content="")
+
+#     async for chunk in runnable.astream(
+#         {"question": message.content},
+#         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+#     ):
+#         await msg.stream_token(chunk)
+
+#     await msg.send()
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Morning routine ideation",
+            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+            icon="/public/idea.svg",
+            ),
+
+        cl.Starter(
+            label="Explain superconductors",
+            message="Explain superconductors like I'm five years old.",
+            icon="/public/learn.svg",
+            ),
+        cl.Starter(
+            label="Python script for daily email reports",
+            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
+            icon="/public/terminal.svg",
+            ),
+        cl.Starter(
+            label="Text inviting friend to wedding",
+            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
+            icon="/public/write.svg",
             )
         ]
-    ).send()
-    value = settings["Model"]
-
-    cl.user_session.set("chat_history", [])
-
-    print(value)
-
-   
-
-
-
-@cl.on_chat_resume
-async def on_chat_resume(thread: ThreadDict):
-    cl.user_session.set("chat_history", [])
-
-    # user_session = thread["metadata"]
-    
-    for message in thread["steps"]:
-        if message["type"] == "user_message":
-            cl.user_session.get("chat_history").append({"role": "user", "content": message["output"]})
-        elif message["type"] == "assistant_message":
-            cl.user_session.get("chat_history").append({"role": "assistant", "content": message["output"]})
-
-
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    chat_history = cl.user_session.get("chat_history")
+    response = requests.post(API_URL, headers=headers, json={"inputs": message.content})
+    output = response.json()
+    if isinstance(output, list) and "generated_text" in output[0]:
 
-    await cl.Message(content="Here is the ticket information!").send()
-    chat_history.append({"role": "admin", "content": message.content})
+        msg = cl.Message(content="")
+        generated_text = output[0]["generated_text"]
+        async for chunk in generate_chunks(generated_text):
+                msg.content += chunk  # Append chunk
+                await msg.update()  # Send updated message
+        await msg.send()
+    else:   
+        await cl.Message(content="Error: Invalid API response").send()
+  
 
-    content = message.content
-    print(content)
-    
+    # print("printing result " + output[0]["generated_text"][0])
+    # await cl.Message(content=output[0]["generated_text"]).send()
+
+# @cl.on_message
+# async def main(message: cl.Message):
+#     response = requests.post(API_URL, headers=headers, json={"inputs": message.content}, stream=True)
+
+#     msg = cl.Message(content="")
+#     for chunk in response.iter_content(chunk_size=1024):  # Stream chunks
+#         msg.content += chunk.decode()
+#         await msg.update()  # Send updates
+
+#     await msg.send()  # Finalize message
