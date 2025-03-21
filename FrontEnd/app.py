f14dc763f465b980c327869837a90439f2ee65ad
Steve Lopez <stivfunk2000@hotmail.com>
2025-03-21 18:59:08 -0500

    agregando frontend con starters

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -9,6 +9,7 @@ import requests
 import os
 
 import chainlit as cl
+
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACE_API_KEY")
 
 API_URL = "https://api-inference.huggingface.co/models/google/gemma-2b"
@@ -25,23 +26,7 @@ def auth_callback(username: str, password: str):
     else:
         return None
     
-@cl.on_chat_start
-async def on_chat_start():
-    
-    # await cl.Message(content="Welcome to the chat!").send()
 
-    model = ChatOpenAI(streaming=True)
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
-            ),
-            ("human", "{question}"),
-        ]
-    )
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
 
 
 async def generate_chunks(text, chunk_size=2000):
@@ -49,43 +34,31 @@ async def generate_chunks(text, chunk_size=2000):
     for i in range(0, len(text), chunk_size):
         yield text[i : i + chunk_size]
 
-# @cl.on_message
-# async def on_message(message: cl.Message):
-#     runnable = cast(Runnable, cl.user_session.get("runnable"))  # type: Runnable
 
-#     msg = cl.Message(content="")
-
-#     async for chunk in runnable.astream(
-#         {"question": message.content},
-#         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-#     ):
-#         await msg.stream_token(chunk)
-
-#     await msg.send()
 
 @cl.set_starters
 async def set_starters():
     return [
         cl.Starter(
-            label="Morning routine ideation",
-            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
-            icon="/public/idea.svg",
+            label="Denuncia siniestro",
+            message="¿Cómo se denuncia un siniestro o accidente?",
+            icon="../public/accident.svg",
             ),
 
         cl.Starter(
-            label="Explain superconductors",
-            message="Explain superconductors like I'm five years old.",
-            icon="/public/learn.svg",
+            label="Calculo gasto reembolsable",
+            message="¿Cómo se calcula un gasto reembolsable?",
+            icon="../public/rembolso.svg",
             ),
         cl.Starter(
-            label="Python script for daily email reports",
-            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
-            icon="/public/terminal.svg",
+            label="Accidente según la póliza POL120190177",
+            message="¿Qué se considera un Accidente según la póliza POL120190177?",
+            icon="../public/poliza.svg",
             ),
         cl.Starter(
-            label="Text inviting friend to wedding",
-            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
-            icon="/public/write.svg",
+            label="No pago de prima",
+            message="¿Qué sucede si el asegurado no paga la prima?",
+            icon="../public/prima.svg",
             )
         ]
 
@@ -106,16 +79,3 @@ async def on_message(message: cl.Message):
         await cl.Message(content="Error: Invalid API response").send()
   
 
-    # print("printing result " + output[0]["generated_text"][0])
-    # await cl.Message(content=output[0]["generated_text"]).send()
-
-# @cl.on_message
-# async def main(message: cl.Message):
-#     response = requests.post(API_URL, headers=headers, json={"inputs": message.content}, stream=True)
-
-#     msg = cl.Message(content="")
-#     for chunk in response.iter_content(chunk_size=1024):  # Stream chunks
-#         msg.content += chunk.decode()
-#         await msg.update()  # Send updates
-
-#     await msg.send()  # Finalize message
